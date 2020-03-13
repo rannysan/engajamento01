653c8247dc7b13eebfb6b40300dbc6e724d1fa56
rannysan <bs.raniel@gmail.com>
2020-03-12 22:48:29 -0300

    refactoring

--- src/main/java/engajamento01/principal.java
+++ src/main/java/engajamento01/principal.java
@@ -1,25 +1,56 @@
 package engajamento01;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class principal {
-   private static ArrayList<alunos> alunos = new ArrayList<alunos>();
+    private static boolean run = true;
+    // Nesse contexto não tem a necessidade de implementar uma classe 
+    //private static ArrayList<alunos> alunos = new ArrayList<alunos>();
+    private static ArrayList<String> alunos = new ArrayList<>();
+    private static Scanner scan = new Scanner(System.in);
 
-    public static ArrayList<alunos> getalunos(){
-        return alunos;
-    }
-
-    public static void adicionar(alunos a){
-        System.out.print("Informe seu nome: ");
-        alunos.add(a);
-    }
-
-    static public String imprimir(){
-        String imp = " ";
-            for(alunos a : alunos){
-                imp += a.imprimir();
+    public static void main(String[] args) {
+        while (run) {
+            showMenu();
+            String option = scan.nextLine();
+            if(option.equals("1")){
+                adicionar();
+            }else if(option.equals("2")){
+                imprimir();
+                run = false;
+            }else{
+                System.out.println("Insira uma opção valida !!!");
             }
-            return imp;
+        }
+    }
+
+    private static void showMenu() {
+        System.out.println("Menu");
+        System.out.println("1 - Inserir estudantes ");
+        System.out.println("2 - Listar Estudantes");
+    }
+
+    /*public static ArrayList<alunos> getalunos() {
+        return alunos;
+    }*/
+
+    public static void adicionar(/*alunos a*/) {
+        System.out.print("Informe seu nome: ");
+        String aluno = scan.nextLine();
+        if(!alunos.contains(aluno)){
+            alunos.add(aluno);
+        }else{
+            System.out.println("Aluno ja cadastrado");
+        }      
+    }
+
+    static public /*String*/void imprimir() {
+        //String imp = " ";
+        for (/*aluno*/ String a : alunos) {
+            //imp += a.imprimir();
+            System.out.println(a);
+        }
+        //return imp;
     }
 }
-    
